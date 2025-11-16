53c13c77573a12932e3c2188817caf750bd536da
David Cardona <dcbapc@gmail.com>
2025-11-16 18:52:42 -0500

    Initial Commit

---
+++ FlashcardsTests/FlashcardsTests.swift
@@ -0,0 +1,17 @@
+//
+//  FlashcardsTests.swift
+//  FlashcardsTests
+//
+//  Created by David Cardona on 11/16/25.
+//
+
+import Testing
+@testable import Flashcards
+
+struct FlashcardsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
